361bd5ed34ca63c72cbf9ac6dce2f95dd5e295ce
Nick <darkforceyoda@gmail.com>
2023-06-24 00:03:49 -0500

    Navbar styling

--- app/components/navbar/MobileNavbar.tsx
+++ app/components/navbar/MobileNavbar.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
-type MobileNavbarProps = {
-    
+type MobileNavbarProps = {};
+
+const MobileNavbar: React.FC<MobileNavbarProps> = () => {
+  return (
+    <nav className="w-full py-8 px-12 lg:hidden flex justify-between items-center">
+      <div className="flex justify-center items-center gap-4">
+        <GiHamburgerMenu size={25} className="text-gray-500" />
+        <h2 className="font-bold text-2xl mr-8 text-black">sneakers</h2>
+      </div>
+      <div className="flex justify-center items-center gap-4">
+        <div className="flex justify-center items-center gap-4">
+          <HiOutlineShoppingCart size={25} />
+          <>icon</>
+        </div>
+      </div>
+    </nav>
+  );
 };
-
-const MobileNavbar:React.FC<MobileNavbarProps> = () => {
-    
-    return <div>Mobile</div>
-}
 export default MobileNavbar;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <nav className="hidden lg:fixed w-full border-b-2 py-8 lg:flex justify-center items-center">
-      <h2 className="font-bold text-2xl mr-8">sneakers</h2>
-      <ul className="flex justify-center items-center gap-4">
-        <li>Collections</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <div className="flex justify-center items-center">
-        <>shopping cart</>
-        <>icon</>
+    <nav className="hidden lg:fixed w-full lg:flex px-24">
+      <div className=" border-b-2 py-8 w-full flex justify-between items-center">
+        <ul className="flex justify-center items-center gap-4 text-gray-400">
+          <h2 className="font-bold text-2xl mr-8 text-black">sneakers</h2>
+          <li className="hover:cursor-pointer">Collections</li>
+          <li className="hover:cursor-pointer">Men</li>
+          <li className="hover:cursor-pointer">Women</li>
+          <li className="hover:cursor-pointer">About</li>
+          <li className="hover:cursor-pointer">Contact</li>
+        </ul>
+        <div className="flex justify-center items-center gap-4">
+          <HiOutlineShoppingCart size={25} />
+          <>icon</>
+        </div>
       </div>
     </nav>
   );
