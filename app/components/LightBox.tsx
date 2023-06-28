a7e5947c22d166c68f496f3736fd303577c5b86e
Nick <darkforceyoda@gmail.com>
2023-06-27 23:49:33 -0500

    LightBox comp created

---
+++ app/components/LightBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type LightBoxProps = {
+    
+};
+
+const LightBox:React.FC<LightBoxProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default LightBox;
