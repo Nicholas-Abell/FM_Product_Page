04fd1a94cef58236d846e3bba3abf0d5df9dc67e
Nick <darkforceyoda@gmail.com>
2023-07-01 23:39:03 -0500

    cart modal for lg screens

--- app/components/cart/Modal.tsx
+++ app/components/cart/Modal.tsx
@@ -4,12 +4,12 @@ type ModalProps = {};
 
 const Modal: React.FC<ModalProps> = () => {
   return (
-    <div className="w-full absolute top-36 left-0 z-30 px-2">
-      <div className="w-full bg-white rounded-lg">
-        <h2 className=" border-b border-gray-400 font-bold p-12 text-2xl">
+    <div className="w-full lg:w-[320px] absolute lg:fixed top-36 lg:top-20 lg:right-0 z-30 px-2">
+      <div className="w-full bg-white rounded-lg shadow-lg shadow-gray-400">
+        <h2 className=" border-b border-gray-400 font-bold p-12 lg:p-2 text-2xl lg:text-base">
           Cart
         </h2>
-        <p className="text-center text-gray-500 py-24 text-2xl">
+        <p className="text-center text-gray-500 py-24 lg:py-6 text-2xl lg:text-base">
           Your cart is empty
         </p>
       </div>
