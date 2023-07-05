b45452d2de99c94d899d4cbe7818e302764d8eb9
Nick <darkforceyoda@gmail.com>
2023-07-04 23:20:54 -0500

    useLocalHost hook, ShoppingCartContext

--- app/components/cart/Modal.tsx
+++ app/components/cart/Modal.tsx
@@ -1,21 +1,22 @@
 "use client";
 import React, { useState } from "react";
 
-type ModalProps = {};
+type ModalProps = { isOpen: boolean };
 
-const Modal: React.FC<ModalProps> = () => {
-  const [] = useState();
+const Modal: React.FC<ModalProps> = ({ isOpen }) => {
   return (
-    <div className="w-full lg:w-[400px] absolute lg:fixed top-36 lg:top-20 lg:right-10 z-30 px-2">
-      <div className="w-full bg-white rounded-lg shadow-lg shadow-gray-400">
-        <h2 className=" border-b border-gray-300 font-bold p-12 lg:p-6 text-2xl lg:text-base">
-          Cart
-        </h2>
-        <p className="text-center text-gray-500 py-24 lg:py-24 text-2xl lg:text-base">
-          Your cart is empty
-        </p>
+    isOpen && (
+      <div className="w-full lg:w-[400px] absolute lg:fixed top-36 lg:top-20 lg:right-10 z-30 px-2">
+        <div className="w-full bg-white rounded-lg shadow-lg shadow-gray-400">
+          <h2 className=" border-b border-gray-300 font-bold p-12 lg:p-6 text-2xl lg:text-base">
+            Cart
+          </h2>
+          <p className="text-center text-gray-500 py-24 lg:py-24 text-2xl lg:text-base">
+            Your cart is empty
+          </p>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 export default Modal;
--- app/context/ShoppingCartContext.tsx
+++ app/context/ShoppingCartContext.tsx
@@ -1,6 +1,9 @@
-import React, { ReactNode, createContext, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { useLocalStorage } from "../Hooks/UseLocalStorage";
 
-type ShoppingCartContextProps = {
+import ShoppingCart from "../components/cart/Modal";
+
+type ShoppingCartProviderProps = {
   children: ReactNode;
 };
 
@@ -24,4 +27,85 @@ const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [isOpen, setIsopen] = useState(false);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "Shopping Cart",
+    []
+  );
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => {
+    setIsopen(true);
+  };
+
+  const closeCart = () => {
+    setIsopen(false);
+  };
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...cartItems, { id, quantity: 1 }];
+      } else {
+        return cartItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return cartItems.filter((item) => item.id !== id);
+      } else {
+        return cartItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currItems) => {
+      return cartItems.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseQuantity,
+        decreaseQuantity,
+        removeFromCart,
+        cartQuantity,
+        cartItems,
+        openCart,
+        closeCart,
+      }}
+    >
+      {children}
+      <ShoppingCart isOpen={isOpen} />
+    </ShoppingCartContext.Provider>
+  );
 }
